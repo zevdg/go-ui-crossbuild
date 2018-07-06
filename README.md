ef29552d39d5f12f280fb08509ed99f9e3dad48b
Zev Goldstein <zev.goldstein@gmail.com>
2018-07-06 17:49:10 -0400

    gouicrossbuild.sh rewrite

--- README.md
+++ README.md
@@ -1,20 +1,55 @@
 # go-ui-crossbuild
+
 Cross compiler for use with the [go ui library](https://github.com/andlabs/ui)  
 Builds amd64 binaries for linux, windows and macOS(darwin)  
-[magj/go-ui-crossbuild on dockerhub](https://hub.docker.com/r/magj/go-ui-crossbuild/)
+[zevdg/go-ui-crossbuild on dockerhub](https://hub.docker.com/r/zevdg/go-ui-crossbuild/)
 
 ### Usage
-1. Mount your project to `/go/src/project_name`
-2. Invoke the `gouicrossbuild` command,  
-   specify the name of your project (to match the mount point) and the path to the module to build.  
-   
-##### Example:
+
+Mount your project to `/go/src/project_root` and invoke the `gouicrossbuild` command
+
+#### Example
+
 ```bash
-docker run -v $GOPATH/project_name:/go/src/project_name magj/go-ui-crossbuild gouicrossbuild project_name ./cmd/gui
+# -u flag creates files as the current user instead of root
+# -e flag fixes issue with go's cache when running as non-root in docker
+docker run -u $(id -u) -e XDG_CACHE_HOME='/tmp/.cache' \
+-v $GOPATH/github.com/my-org/my-project:/go/src/project_root \
+zevdg/go-ui-crossbuild gouicrossbuild [options]
 ```
 
-## License
+### gouicrossbuild usage
+
+`gouicrossbuild [-s src_directory] [-p package_directory] [-o output_directory] [go_build_args...]`
+
+#### Options
+
+All paths must be relative to the project root
+
+**src_directory** (default '.')  
+The root source folder  
+Must contain a vendor directory that includes "github.com/andlabs/ui"  
+This parameter will be deprecated once https://github.com/andlabs/ui/issues/230 is fixed
+
+**package_directory** (default '.')  
+ The directory containing the package to be built
+
+**output_directory** (default '.')  
+ Where to save the build output
+
+#### Examples
+
+```
+# build the package at <project_root> with vendor directory <project_root>/vendor
+# output to <project_root>
+gouicrossbuild
+
+# build the package at <project_root>/src/cmd/gui with vendor directory <project_root>/src/vendor
+# output to <project_root>/bin
+gouicrossbuild -s src -p src/cmd/gui -o bin
+```
+
+## Licensing info
+
 Building for macOS uses code from the Apple SDK  
 [Please read and understand the Apple Xcode and SDK license before using.](https://www.apple.com/legal/sla/docs/xcode.pdf)
-
-Any original code is licenced under "unlicense"
--- gouicrossbuild.sh
+++ gouicrossbuild.sh
@@ -1,42 +1,82 @@
-#!/bin/sh
-die () {
-    printf >&2 "$@"
+#!/bin/bash
+
+PACKAGE_DIR="."
+OUTPUT_DIR="."
+SRC_DIR="."
+
+read -r -d '' USAGE << EOM
+Usage: gouicrossbuild [-s src_directory] [-p package_directory] [-o output_directory] [go_build_args...]
+
+Options:
+    All paths must be relative to the project root
+
+    src_directory (default '$SRC_DIR')
+        The root source folder
+        Must contain a vendor directory that includes "github.com/andlabs/ui"
+        This parameter will be deprecated once https://github.com/andlabs/ui/issues/230 is fixed
+
+    package_directory (default '$PACKAGE_DIR')
+        The directory containing the package to be built
+
+    output_directory (default '$OUTPUT_DIR')
+        Where to save the build output
+
+Examples:
+    # build the package at <project_root> with vendor directory <project_root>/vendor
+    # output to <project_root>
+    gouicrossbuild
+
+    # build the package at <project_root>/src/cmd/gui with vendor directory <project_root>/src/vendor
+    # output to <project_root>/bin
+    gouicrossbuild -s src -p src/cmd/gui -o bin
+EOM
+
+while getopts "s:p:o:h" FLAG
+do	case "$FLAG" in
+	s) # Remove when https://github.com/andlabs/ui/issues/230 fixed
+        SRC_DIR="$OPTARG"
+        ;;
+	p)
+        PACKAGE_DIR="$OPTARG"
+        ;;
+	o)
+        OUTPUT_DIR="$OPTARG"
+        ;;
+    h)
+        echo "$USAGE"
+        exit 0
+        ;;
+	[?])
+        printf >&2 "Error: Invalid Flag\n\n$USAGE"
+        exit 2
+        ;;
+	esac
+done
+shift $((OPTIND -1))
+
+# Copy locally compiled libui to avoid linking error
+# Remove when https://github.com/andlabs/ui/issues/230 fixed
+UI_VENDOR_DIR=$SRC_DIR/vendor/github.com/andlabs/ui
+if [[ ! -d $UI_VENDOR_DIR ]]; then
+    printf >&2 "'$UI_VENDOR_DIR' expected but not found (relative to project root)"
     exit 1
-}
-if (( $# < 2 )); then die "Provide a project root path and a relative package path \nusage: github.com/name/project ./cmd/gui binary_output_path"; fi
-PROJECT_ROOT=$1
-PACKAGE=$2
-PACKAGE_BASENAME=`basename ${PACKAGE}| cut -f 1 -d '.'`
-if [ -n "$1" ]; then
-    mkdir -p $3
-    BUILD_OUTPUT="${3}/${PACKAGE_BASENAME}"
-else
-    BUILD_OUTPUT="${PACKAGE_BASENAME}"
 fi
+cp /tmp/libui_linux_amd64.a "$UI_VENDOR_DIR"
+# End of section to remove
 
-shift
-shift
-shift
-cd ${PROJECT_ROOT}
 export GOARCH=amd64
+export CGO_ENABLED=1
+echo "Building linux binary"
+GOOS=linux \
+CC=clang CXX=clang++ \
+go build -o $OUTPUT_DIR/linux ${PACKAGE_DIR} $*
 
-# if not invoked via gouicrossbuild just do a simple go build
-if [ `basename $0` == "gouicrossbuild" ]; then
-    export CGO_ENABLED=1
-    # Copy locally compiled libui to avoid linking error
-    # Remove when https://github.com/andlabs/ui/issues/230 fixed
-    cp /tmp/libui_linux_amd64.a vendor/github.com/andlabs/ui
-    echo "Building linux binary"
-    GOOS=linux CC=clang CXX=clang++ go build -o ${BUILD_OUTPUT}_linux ${PACKAGE} $*
-    echo "Building windows binary"
-    GOOS=windows CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o ${BUILD_OUTPUT}_windows.exe -ldflags "-H=windowsgui -extldflags=-s" ${PACKAGE} $*
-    echo "Building darwin binary"
-    GOOS=darwin CGO_LDFLAGS_ALLOW="-mmacosx-version-min.*" CC=o64-clang CXX=o64-clang++ go build -o ${BUILD_OUTPUT}_darwin.app ${PACKAGE} $*
-else
-    echo "Building linux binary"
-    GOOS=linux go build -o ${BUILD_OUTPUT}_linux ${PACKAGE} $*
-    echo "Building windows binary"
-    GOOS=windows go build -o ${BUILD_OUTPUT}_windows.exe ${PACKAGE} $*
-    echo "Building darwin binary"
-    GOOS=darwin go build -o ${BUILD_OUTPUT}_darwin ${PACKAGE} $*
-fi
+echo "Building windows binary"
+GOOS=windows \
+CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ \
+go build -o $OUTPUT_DIR/windows.exe -ldflags "-H=windowsgui -extldflags=-s" ${PACKAGE_DIR} $*
+
+echo "Building darwin binary"
+GOOS=darwin \
+CGO_LDFLAGS_ALLOW="-mmacosx-version-min.*" CC=o64-clang CXX=o64-clang++ \
+go build -o $OUTPUT_DIR/darwin.app ${PACKAGE_DIR} $*
